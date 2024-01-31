8b82d7aaac448ac834dfe6d2a2b57c6ac5529a1e
fleedom88 <fleedom88@gmail.com>
2024-01-31 12:59:56 +0900

    함수형 컴포넌트로 변경중

--- my-app/src/components/Board.js
+++ my-app/src/components/Board.js
@@ -1,47 +1,43 @@
-import React, { Component } from 'react'
+import React, { useState} from 'react'
 import Square from './Square'
 import './Board.css'
 
-export default class Board extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-          squares:Array(9).fill(null) //Square.js의 state가져옴
-        }
+const Board = () =>{
+    const [squares,setSquares] = useState(Array(9).fill(null));
+
+    const handleClick = () => {
+      const newSquares = squares.slice(); //생성자의 state
+      newSquares[i] = 'X';
+      setSquares(newSquares);
     }
 
-    handleClick(i){
-      const squares = this.state.squares.slice(); //생성자의 state
-      squares[i] = 'X';
-      this.setState({squares:squares});
-    }
-
-    renderSquare(i){
+    const renderSquare = (i) =>{
         return <Square value={this.state.squares[i]} 
-        onClick={()=> this.handleClick(i)} />        
+        onClick={()=> handleClick(i)} />        
     }
-
-
-  render() {
+ 
     return (
       <div>
         <div className='status'>Next Player: X, O</div>
         <div className='board-row'>
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-            {this.renderSquare(3)}
+            {renderSquare(1)}
+            {renderSquare(2)}
+            {renderSquare(3)}
         </div>
         <div className='board-row'>
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-            {this.renderSquare(6)}
+            {renderSquare(4)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+            {renderSquare(6)}
         </div>
         <div className='board-row'>
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-            {this.renderSquare(9)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+            {renderSquare(9)}
         </div>
       </div>
     )
-  }
+  
 }
+
+export default Board;
