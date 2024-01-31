aa1529665142765e6219a1be93cc57601977c9d6
FREEDOM <fleedom88>
2024-01-31 23:46:54 +0900

    승자가 안나오는 틱택토

--- my-app/src/components/Board.js
+++ my-app/src/components/Board.js
@@ -1,31 +1,71 @@
 import React, { useState} from 'react'
-import Square from './Square'
+import Square from './Square.js'
 import './Board.css'
 
 const Board = () =>{
     const [squares,setSquares] = useState(Array(9).fill(null));
+    const [isX,setIsX] = useState(true);
+    const calculateWinner = (squares)=>{
+      const lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+      ]
+      for (let index = 0; index < lines.length; index++) {
+        const [a,b,c] = lines[index];
+        if (squares[a] && squares[a] === squares[b] &&squares[a] === squares[c]) {
+          return squares[a];
+        }
+        return null;              
+      }
+    }
 
-    const handleClick = () => {
-      const newSquares = squares.slice(); //생성자의 state
-      newSquares[i] = 'X';
+    const winner = calculateWinner(squares);
+    let status ='';
+
+    if (winner) {
+      status = 'Winner: ' + winner;
+    }
+    else{
+      status = `Next Player : ${isX?'X':'O'}`;
+    }
+
+    const handleClick = (i) => {
+      const newSquares = squares.slice(); //생성자의 state     
+      if(calculateWinner(newSquares) || newSquares[i]){
+        return;
+      }
+      newSquares[i] = isX ?' X':'O';
       setSquares(newSquares);
+      // isX?setIsX(false):setIsX(true);
+      setIsX(!isX);
+      // setIsX(prev => !prev);
+      
     }
 
     const renderSquare = (i) =>{
-        return <Square value={this.state.squares[i]} 
+        return <Square value={squares[i]} 
         onClick={()=> handleClick(i)} />        
     }
- 
+    
+    const clearSquares=()=>{
+      setSquares(Array(9).fill(null));
+    }
+
     return (
       <div>
-        <div className='status'>Next Player: X, O</div>
+        <div className='status'>{status}</div>
         <div className='board-row'>
             {renderSquare(1)}
             {renderSquare(2)}
             {renderSquare(3)}
         </div>
         <div className='board-row'>
-            {renderSquare(4)}
             {renderSquare(4)}
             {renderSquare(5)}
             {renderSquare(6)}
@@ -35,6 +75,8 @@ const Board = () =>{
             {renderSquare(8)}
             {renderSquare(9)}
         </div>
+        <br/>
+        <button onClick={clearSquares}>clear</button>
       </div>
     )
   
